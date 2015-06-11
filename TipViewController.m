cc962707cadfeee6ae4371deab2fa9cf15ced60f
Jeff Tang <jetang@yahoo-inc.com>
2015-06-12 02:21:11 +0800

    layout

--- TipViewController.m
+++ TipViewController.m
@@ -14,6 +14,14 @@
 
 @implementation TipViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = @"Tip Calculator";
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
